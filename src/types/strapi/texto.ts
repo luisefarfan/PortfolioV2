b61a2043c39981990ff6de77f50dff284a1da738
LuisFarfan57 <luise.prado99@hotmail.com>
2024-10-03 21:02:33 -0600

    feat: add dynamic content

---
+++ src/types/strapi/texto.ts
@@ -0,0 +1,11 @@
+export interface Texto {
+  id: number;
+  documentId: string;
+  contenido: string;
+  nombreInterno: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  locale: null;
+  ubicacion: string;
+}
