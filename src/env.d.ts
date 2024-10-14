00d33c3824103ada04a73c91aa15100bc455fc18
LuisFarfan57 <luise.prado99@hotmail.com>
2024-10-13 20:47:08 -0600

    feat: implement contentful

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,7 @@
 
 interface ImportMetaEnv {
   readonly STRAPI_API_TOKEN: string;
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
 }
