112284b097e34ec2b68d9c57f3a9fa35041f9c03
LuisFarfan57 <luise.prado99@hotmail.com>
2024-12-21 15:22:37 -0600

    feat: daisyUI and layout

---
+++ src/content/constants.ts
@@ -0,0 +1,26 @@
+export const constants = {
+  menu: {
+    links: [
+      {
+        es: 'Sobre mi',
+        en: 'About me'
+      },
+      {
+        es: 'Experiencia',
+        en: 'Experience'
+      },
+      {
+        es: 'Proyectos',
+        en: 'Projects'
+      },
+      {
+        es: 'Habilidades',
+        en: 'Skills'
+      },
+      {
+        es: 'Contacto',
+        en: 'Contact'
+      }
+    ]
+  }
+}
