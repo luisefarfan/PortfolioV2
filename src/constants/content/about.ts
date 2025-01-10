676230f7bbbbc5d1d8587d659799df635c331a57
LuisFarfan57 <luise.prado99@hotmail.com>
2025-01-09 21:28:32 -0600

    feat: add about

---
+++ src/constants/content/about.ts
@@ -0,0 +1,12 @@
+export const aboutContent = {
+  en: {
+    title1: 'About',
+    title2: 'Me',
+    content: 'I am a highly skilled software engineer with more than 4 years of experience as a full-stack developer, specializing in Javascript frameworks and libraries like React, NodeJS, and NextJS. I am passionate about crafting clean, efficient, and scalable solutions and have gained valuable experience in developing complex applications, implementing secure APIs, and optimizing performance for high-traffic websites. Additionally, I have experience conducting code reviews and mentoring other developers. I also deeply understand the business and how technology can be leveraged to create the most value. I am eager to leverage my skills and experience to help teams build innovative solutions and drive business growth.'
+  },
+  es: {
+    title1: 'Sobre',
+    title2: 'Mi',
+    content: 'Soy un ingeniero de software altamente capacitado con más de 4 años de experiencia como desarrollador full-stack, especializado en frameworks y bibliotecas de Javascript como React, NodeJS y NextJS. Me apasiona crear soluciones limpias, eficientes y escalables, y he adquirido una valiosa experiencia en el desarrollo de aplicaciones complejas, la implementación de APIs seguras y la optimización del rendimiento para sitios web de alto tráfico. Además, tengo experiencia en la realización de revisiones de código y en la mentoría de otros desarrolladores. También comprendo profundamente el negocio y cómo la tecnología puede ser aprovechada para crear el máximo valor. Estoy ansioso por aprovechar mis habilidades y experiencia para ayudar a los equipos a construir soluciones innovadoras y impulsar el crecimiento del negocio.'
+  }
+}
