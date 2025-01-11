d9e26a03b2cf235d81e0a1c7a6d9cbdcdb131967
LuisFarfan57 <luise.prado99@hotmail.com>
2025-01-10 22:55:14 -0600

    feat: add contact section

---
+++ src/constants/content/contact.ts
@@ -0,0 +1,28 @@
+export const contactContent = {
+  en: {
+    title1: 'Let\'s Keep',
+    title2: 'in Touch'
+  },
+  es: {
+    title1: 'Sigamos en',
+    title2: 'Contacto'
+  },
+  contactMenuContent: {
+    phone: {
+      value: '+50242980332',
+      label: 'Phone'
+    },
+    email: {
+      value: 'luisefarfan.dev@gmail.com',
+      label: 'Email'
+    },
+    linkedin: {
+      value: 'https://www.linkedin.com/in/luisefarfan/',
+      label: 'LinkedIn'
+    },
+    github: {
+      value: 'https://github.com/luisefarfan',
+      label: 'Github'
+    },
+  }
+}
