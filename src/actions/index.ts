35e0414f8df8b2c55c2549c056cd6fabdd6f68a0
LuisFarfan57 <luise.prado99@hotmail.com>
2025-07-26 12:26:09 -0600

    feat: Implement email sending functionality with form validation in Contact section

---
+++ src/actions/index.ts
@@ -0,0 +1,5 @@
+import { sendEmailAction } from "./sendEmail";
+
+export const server = {
+  sendEmail: sendEmailAction
+}
---
+++ src/actions/sendEmail.ts
@@ -0,0 +1,65 @@
+import { defineAction } from "astro:actions";
+import { z } from 'astro:schema';
+import { Resend } from "resend";
+
+const getEmailMessage = (name: string, message: string, lang: 'en' | 'es'): string => {
+  const emailMessages = {
+    es: `
+      <p>Hola ${name},</p>
+      <p>Gracias por ponerte en contacto! Aquí tienes una copia de tu mensaje:</p>
+      <blockquote>${message}</blockquote>
+      <p>Me pondré en contacto contigo lo antes posible.</p>
+    <p>Saludos,<br />Luis Enrique Farfán</p>
+  `,
+    en: `
+    <p>Hi ${name},</p>
+    <p>Thank you for reaching out! Here's a copy of your message:</p>
+    <blockquote>${message}</blockquote>
+    <p>I will get back to you as soon as possible.</p>
+    <p>Best regards,<br />Luis Enrique Farfán</p>
+  `
+  }
+
+  return emailMessages[lang];
+}
+
+export const sendEmailAction = defineAction({
+  input: z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email address"),
+    message: z.string().min(1, "Message is required"),
+    lang: z.literal('es').or(z.literal('en'))
+  }),
+  handler: async (input) => {
+    const { name, email, message, lang } = input;
+
+    console.log("Sending email with input:", { name, email, message, lang });
+
+    try {
+      const resendClient = new Resend(import.meta.env.RESEND_API_KEY);
+
+      const emailMessage = getEmailMessage(name, message, lang);
+      const { data, error } = await resendClient.emails.send({
+        from: 'Luis Enrique Farfán <dev@luisefarfan.com>',
+        to: email,
+        subject: 'Thank you for contacting me',
+        html: emailMessage
+      })
+
+      if (error) {
+        console.error("Resend error sending email:", error);
+        throw new Error(error.message);
+      }
+
+      console.log("Email sent successfully:", data);
+      return {
+        success: true,
+        message: "Email sent successfully"
+      }
+    }
+    catch (error) {
+      console.error("Unexpected error sending email:", error);
+      throw new Error("Failed to send email");
+    }
+  }
+})
